2dec6e6d8df5a6cde1a50c32f392660c55893380
Nathaniel Nanle <nathanielnanle100@gmail.com>
2024-09-04 13:34:28 +0100

    NFT Whitelist

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,17 +2,52 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Whitelist} from "../src/Whitelist.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract WhiteListScript is Script {
+    Whitelist public whitelist;
 
     function setUp() public {}
 
     function run() public {
+
+        address[] addresses = [](10);
+        
+        address user1 = vm.makeAddr("user1");
+        addresses.push(user1);
+        
+        address user2 = vm.makeAddr("user2");
+        addresses.push(user2);
+        
+        address user3 = vm.makeAddr("user3");
+        addresses.push(user3);
+        
+        address user4 = vm.makeAddr("user4");
+        addresses.push(user4);
+        
+        address user5 = vm.makeAddr("user5");
+        addresses.push(user5);
+        
+        address user6 = vm.makeAddr("user6");
+        addresses.push(user6);
+        
+        address user7 = vm.makeAddr("user7");
+        addresses.push(user7);
+        
+        address user8 = vm.makeAddr("user8");
+        addresses.push(user8);
+        
+        address user9 = vm.makeAddr("user9");
+        addresses.push(user9);
+        
+        address user10 = vm.makeAddr("user10");
+        addresses.push(user10);
+
+
+
         vm.startBroadcast();
 
-        counter = new Counter();
+        whitelist = new Whitelist(addresses);
 
         vm.stopBroadcast();
     }
